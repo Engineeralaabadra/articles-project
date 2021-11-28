933c25ab818a19d8505b9b3db50fcccee57410d4
alaabadra <alaabadra4@gmail.com>
2021-11-28 18:00:21 +0200

    in ArticleServiceProvider in modules : define routes of folders,files that it in module via these methods loadRoutesFrom,loadViewsFrom,loadMigrationsFrom,loadTranslationsFrom-> for files, folders that it in routes,resources,databse and getRequire-> for file in config folder

---
+++ app/modules/articels/Providers/ArticleServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Articles\Providers;
+
+use Illuminate\Support\Facades\File as FacadesFile;
+use Illuminate\Support\ServiceProvider;
+
+class ArticleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $ds=DIRECTORY_SEPARATOR;
+        $moduleName='articles';
+        config([
+            'articles'=>FacadesFile::getRequire(__DIR__.$ds.'..'.$ds.'config'.$ds.'route.php')
+            ]);
+       $this->loadRoutesFrom(__DIR__.$ds.'..'.$ds.'routes'.$ds.'admin'.$ds.'api.php');
+       $this->loadRoutesFrom(__DIR__.$ds.'..'.$ds.'routes'.$ds.'admin'.$ds.'web.php');
+       $this->loadViewsFrom(__DIR__.$ds.'..'.$ds.'resources'.$ds.'views',$moduleName);
+       $this->loadMigrationsFrom(__DIR__.$ds.'..'.$ds.'database'.$ds.'migrations',$moduleName);
+       $this->loadTranslationsFrom(__DIR__.$ds.'..'.$ds.'resources'.$ds.'lang',$moduleName);
+    }
+}
