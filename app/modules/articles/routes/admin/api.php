06681d637c68302a0d24d4582c14149da9237ff6
alaabadra <alaabadra4@gmail.com>
2021-11-28 23:26:15 +0200

    add the main routes for article admin : index,show,update,destroy

--- app/modules/articles/routes/admin/api.php
+++ app/modules/articles/routes/admin/api.php
@@ -5,9 +5,12 @@ Route::prefix('api')->group(function(){
     // Route::get('/index',function(){
     //     return 88;
     // });
-    Route::namespace('Articles\Http\Controllers')->middleware('article')->group(function(){
+    Route::namespace('Articles\Http\Controllers')->group(function(){
         Route::prefix(config('articles.prefix.admin',config('module.prefix.admin')).'/'.config('articles.module-name'))->namespace('Admin')->group(function(){
             Route::get('index','ArticleController@index');
+            Route::get('show/{id}','ArticleController@show');
+            Route::post('update/{id}','ArticleController@update');
+            Route::get('destroy/{id}','ArticleController@destroy');
         });
 });
 // Route::get('/index',"Articles\Http\Controllers\Admin\ArticleController@index");
