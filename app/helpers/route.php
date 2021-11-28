6963c5cf0f54d80ad3406a8fe44b6a1886cc4b74
alaabadra <alaabadra4@gmail.com>
2021-11-29 01:30:05 +0200

    add method for enhancement for namespace in api for this module article

--- app/helpers/route.php
+++ app/helpers/route.php
@@ -8,3 +8,11 @@ function buildPrefix(string $moduleName,string $type='admin'){
     // config('articles.prefix.admin',config('module.prefix.admin')).'/'.config('articles.module-name');
   return  config($moduleName.'.prefix.admin',config('module.prefix.'.$type)).'/'.config($moduleName.'.module-name');
 }
+/**
+ * @param string $moduleName
+ * @return string
+ */
+function buildControllerNamespace(string $moduleName){
+  // return ucfirst($moduleName).'Http\Controllers';
+  return 'Articles\Http\Controllers';
+}
