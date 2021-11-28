f6d1bb26427d53f2170a180e54242c0737f46833
alaabadra <alaabadra4@gmail.com>
2021-11-28 22:08:02 +0200

    use methods that it for enhancement in file ArticleServiceProvderloadRoute , loadViews,loadMigrations, loadTranslations

--- app/modules/articles/Providers/ArticleServiceProvider.php
+++ app/modules/articles/Providers/ArticleServiceProvider.php
@@ -29,11 +29,17 @@ class ArticleServiceProvider extends ServiceProvider
         config([
             'articles'=>FacadesFile::getRequire(__DIR__.$ds.'..'.$ds.'config'.$ds.'route.php')
             ]);
-       $this->loadRoutesFrom(__DIR__.$ds.'..'.$ds.'routes'.$ds.'admin'.$ds.'api.php');
-       $this->loadRoutesFrom(__DIR__.$ds.'..'.$ds.'routes'.$ds.'admin'.$ds.'web.php');
-       $this->loadViewsFrom(__DIR__.$ds.'..'.$ds.'resources'.$ds.'views',$moduleName);
-       $this->loadMigrationsFrom(__DIR__.$ds.'..'.$ds.'database'.$ds.'migrations',$moduleName);
-       $this->loadTranslationsFrom(__DIR__.$ds.'..'.$ds.'resources'.$ds.'lang',$moduleName);
+    //    $this->loadRoutesFrom(__DIR__.$ds.'..'.$ds.'routes'.$ds.'admin'.$ds.'api.php');
+    //    $this->loadRoutesFrom(__DIR__.$ds.'..'.$ds.'routes'.$ds.'admin'.$ds.'web.php');
+    //    $this->loadViewsFrom(__DIR__.$ds.'..'.$ds.'resources'.$ds.'views',$moduleName);
+    //    $this->loadMigrationsFrom(__DIR__.$ds.'..'.$ds.'database'.$ds.'migrations',$moduleName);
+    //    $this->loadTranslationsFrom(__DIR__.$ds.'..'.$ds.'resources'.$ds.'lang',$moduleName);
+        $this->loadRoutesFrom(loadRoute('api',$moduleName));
+        $this->loadRoutesFrom(loadRoute('web',$moduleName));
+
+        $this->loadViewsFrom(loadViews($moduleName),$moduleName);
+        $this->loadMigrationsFrom(loadMigrations($moduleName));
+        $this->loadTranslationsFrom(loadTranslations($moduleName),$moduleName);
     }
 }
 
