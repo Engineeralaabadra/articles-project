ebd8a39ec806c262238f4f242194925a241d7105
alaabadra <alaabadra4@gmail.com>
2021-11-28 22:00:36 +0200

    add ->middleware('article') in api.php for this module article

--- app/modules/articles/routes/admin/api.php
+++ app/modules/articles/routes/admin/api.php
@@ -5,7 +5,7 @@ Route::prefix('api')->group(function(){
     // Route::get('/index',function(){
     //     return 88;
     // });
-    Route::namespace('Articles\Http\Controllers')->group(function(){
+    Route::namespace('Articles\Http\Controllers')->middleware('article')->group(function(){
         Route::prefix(config('articles.prefix.admin',config('module.prefix.admin')).'/'.config('articles.module-name'))->namespace('Admin')->group(function(){
             Route::get('index','ArticleController@index');
         });
