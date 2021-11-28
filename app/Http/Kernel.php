21f2304c4245526a4c9a4cce7a77703ec05d518f
alaabadra <alaabadra4@gmail.com>
2021-11-28 21:55:17 +0200

    define the middleware Article in Kernel.php -> 'article'=>Article\Http\Middleware\Article::class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'article'=>Article\Http\Middleware\Article::class
+
+    
     ];
 }
