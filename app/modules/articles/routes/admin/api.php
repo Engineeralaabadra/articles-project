bb717bf7667263ebad07cd02f4376d11ab5cfaa9
alaabadra <alaabadra4@gmail.com>
2021-11-29 01:31:42 +0200

    add buildControllerNamespace this method to make more enhancement for namespace

--- app/modules/articles/routes/admin/api.php
+++ app/modules/articles/routes/admin/api.php
@@ -5,13 +5,15 @@ Route::prefix('api')->group(function(){
     // Route::get('/index',function(){
     //     return 88;
     // });
-    Route::namespace('Articles\Http\Controllers')->group(function(){
-        Route::prefix(config('articles.prefix.admin',config('module.prefix.admin')).'/'.config('articles.module-name'))->namespace('Admin')->group(function(){
-            Route::get('index','ArticleController@index');
-            Route::get('show/{id}','ArticleController@show');
-            Route::post('update/{id}','ArticleController@update');
-            Route::get('destroy/{id}','ArticleController@destroy');
-        });
+    // Route::namespace('Articles\Http\Controllers')->group(function(){
+        $moduleName='articles';
+        Route::namespace(buildControllerNamespace(getModuleName($moduleName)))->group(function() {
+            Route::prefix(config('articles.prefix.admin',config('module.prefix.admin')).'/'.config('articles.module-name'))->namespace('Admin')->group(function(){
+                Route::get('index','ArticleController@index');    
+                Route::get('show/{id}','ArticleController@show');
+                Route::post('update/{id}','ArticleController@update');
+                Route::get('destroy/{id}','ArticleController@destroy');
+            });
 });
 // Route::get('/index',"Articles\Http\Controllers\Admin\ArticleController@index");
  });
