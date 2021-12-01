707a812c51d22005ddde6bc1fbbea86e9ef3c8f4
alaabadra <alaabadra4@gmail.com>
2021-12-01 22:19:22 +0200

    install package vuejsDialog and install for it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,8 +9,13 @@ import axios from "axios";
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import "@fortawesome/fontawesome-free/css/all.css";
+import VuejsDialog from 'vuejs-dialog';
+ 
+// include the default style
+import 'vuejs-dialog/dist/vuejs-dialog.min.css';
 
 Vue.use(VueSidebarMenu)
+Vue.use(VuejsDialog);
 window.axios = axios;
 Vue.config.productionTip = false;
 
